3b9d98ef59f64227f12e32d7f6a6ff38278f8062
wudi <1113249273@qq.com>
2017-07-09 14:25:44 +0800

    main

--- Application/Runtime/Cache/Tool/ce72bd9ed436d82d246c177884db825d.php
+++ Application/Runtime/Cache/Tool/ce72bd9ed436d82d246c177884db825d.php
@@ -1,17 +1,1 @@
-<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
-	<title>工具界-CssNb</title>
-	<meta name="keywords" content="在线,CSS,CSS 校验,格式化,xml转json 工具,在线工具,json视图,可视化,程序,服务器,域名注册,正则表达式,测试,在线json格式化工具,json 格式化,json格式化工具,json字符串格式化,json 在线查看器,json在线,json 在线验证,json tools online,在线文字对比工具" />
-		<meta name="description" content="在线,CSS,CSS 校验,格式化,xml转json 工具,在线工具,json视图,可视化,程序,服务器,域名注册,正则表达式,测试,在线json格式化工具,json 格式化,json格式化工具,json字符串格式化,json 在线查看器,json在线,json 在线验证,json tools online,在线文字对比工具">
-	<script src="//cdn.bootcss.com/jquery/1.9.1/jquery.min.js"></script>
-	<link href="//cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
-	<script src="//cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-	<!--<script src="http://7bv9ya.com1.z0.glb.clouddn.com/jquery.cookie.js"></script>-->
-</head>
-<body>
-	<!-- <script>alert(1)</script> -->
-</body>
-</html>
+<?php if (!defined('THINK_PATH')) exit();?>
