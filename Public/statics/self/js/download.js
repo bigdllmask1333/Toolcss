405a4a2d56faee65025eaef2b8f838ac7b9df159
1132w11 <1113249273@qq.com>
2017-07-22 10:43:12 +0800

    1

--- Public/statics/self/js/download.js
+++ Public/statics/self/js/download.js
@@ -111,3 +111,281 @@ function d16(){
 	  title:'Xshell5&Xftp5下载链接',
 	});
 }
+function d17(){
+	layer.alert('链：http://pan.baidu.com/s/1jIqpzXc    密：au8m', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'Notepad++7.3.1下载链接',
+	});
+}	
+function d18(){
+	layer.alert('链：http://pan.baidu.com/s/1kVOJziZ    密：sr48', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'SublimeText3汉化版下载链接',
+	});
+}	
+function d19(){
+	layer.alert('链：http://pan.baidu.com/s/1chIwhC    密：vwv1', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'Zend Studio 13.0.1下载链接',
+	});
+}	
+function d20(){
+	layer.alert('链：http://pan.baidu.com/s/1bphGv6J   密：59ui', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'FireBug 2.0.18下载链接',
+	});
+}	
+
+function d21(){
+	layer.alert('链：http://pan.baidu.com/s/1geRZhuV   密：hdu6', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'PhpStorm 10.0.1下载链接',
+	});
+}
+function d22(){
+	layer.alert('链：http://pan.baidu.com/s/1eRJTDrc   密：wcth', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'SublimeText3 Mac版下载链接',
+	});
+}
+function d23(){
+	layer.alert('链：http://pan.baidu.com/s/1i4AZYGP   密：an49', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'ZendStudio 13.5.1 Mac下载链接',
+	});
+}
+function d24(){
+	layer.alert('链：http://pan.baidu.com/s/1bps5BND   密：bygt', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'WebStorm Mac版下载链接',
+	});
+}
+function d25(){
+	layer.alert('链：http://pan.baidu.com/s/1kVQLmth   密：aw9q', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'SQLPro for Mysql1.0.13下载链接',
+	});
+}
+function d26(){
+	layer.alert('链：http://pan.baidu.com/s/1jI5O6fs   密：r7it', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'PhpStorm 10.0.1 Mac版下载链接',
+	});
+}
+
+function d27(){
+	layer.alert('链：http://pan.baidu.com/s/1eSck4aI   密：k73r', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'browser-sync-master下载链接',
+	});
+}
+function d28(){
+	layer.alert('链：http://pan.baidu.com/s/1eR4u8Rw   密：i25a', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'Atom编辑器mac版下载链接',
+	});
+}
+
+function d29(){
+	layer.alert('链：http://pan.baidu.com/s/1hsf4ie4   密：dxf2', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'Dreamweaver Mac版下载链接',
+	});
+}
+
+function d30(){
+	layer.alert('链：http://pan.baidu.com/s/1pKRcRYr   密：eh9g', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'Dreamweaver CS6下载链接',
+	});
+}
+function d31(){
+	layer.alert('链：http://pan.baidu.com/s/1c2ImcCC   密：m5z8', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'FileZilla 3.24下载链接',
+	});
+}
+function d32(){
+	layer.alert('链：http://pan.baidu.com/s/1jIeKk7s   密：jsaj', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'FlashFXP 5.4下载链接',
+	});
+}
+function d33(){
+	layer.alert('链：http://pan.baidu.com/s/1kUHbBZh   密：cz5l', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'git 2.7.2下载链接',
+	});
+}
+function d34(){
+	layer.alert('链：http://pan.baidu.com/s/1c92HHG   密：i2w6', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'PS CS6绿色精简版下载链接',
+	});
+}
+
+function d35(){
+	layer.alert('链：http://pan.baidu.com/s/1i47CqET   密：7kot', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'TortoiseSVN 1.9.5下载链接',
+	});
+}
+function d36(){
+	layer.alert('链：http://pan.baidu.com/s/1bpiwruf   密：4629', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'Yummy FTP MAC版下载链接',
+	});
+}
+function d37(){
+	layer.alert('链：http://pan.baidu.com/s/1slFdJKL   密：d01v', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'谷歌浏览器nofollow插件下载链接',
+	});
+}
+function d38(){
+	layer.alert('链：http://pan.baidu.com/s/1i4FJLU9   密：ea7n', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'Apache 2.2.25下载链接',
+	});
+}
+function d39(){
+	layer.alert('链：http://pan.baidu.com/s/1kUEZHH1   密：btx9', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'MAMP Pro for Mac 3.5下载链接',
+	});
+}
+function d40(){
+	layer.alert('链：http://pan.baidu.com/s/1hstHwfq   密：8elj', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'MYSQL 5.7.12下载链接',
+	});
+}
+function d41(){
+	layer.alert('链：http://pan.baidu.com/s/1qYLRxco   密：w5ry', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'NavicatPremium Mac版下载链接',
+	});
+}
+function d42(){
+	layer.alert('链：http://pan.baidu.com/s/1eS7YBBW   密：lxtp', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'Parallels Desktop 12下载链接',
+	});
+}
+function d43(){
+	layer.alert('链：http://pan.baidu.com/s/1qXD3B8C   密：9xnh', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'PHP 7.0.10下载链接',
+	});
+}
+function d44(){
+	layer.alert('链：http://pan.baidu.com/s/1hs7o8Qg   密：e9bi', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'phpMyAdmin 4.6.6下载链接',
+	});
+}
+function d45(){
+	layer.alert('链：http://pan.baidu.com/s/1jIECeJc   密：d1cu', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'phpStudy下载链接',
+	});
+}
+function d46(){
+	layer.alert('链：http://pan.baidu.com/s/1gfh8k51   密：n8qb', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'php集成环境下载链接',
+	});
+}
+function d47(){
+	layer.alert('链：http://pan.baidu.com/s/1nvSdcHb   密：1q90', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'Python 2.7下载链接',
+	});
+}
+function d48(){
+	layer.alert('链：http://pan.baidu.com/s/1dFMnInB   密：pszo', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'Python 3.5.1 Windows下载链接',
+	});
+}
+function d49(){
+	layer.alert('链：http://pan.baidu.com/s/1o7Ktmt0   密：w5ea', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'UPUPW Apache(32位)下载链接',
+	});
+}
+function d50(){
+	layer.alert('链：http://pan.baidu.com/s/1eRDOaAm   密：t5kw', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'UPUPW apache(64位)下载链接',
+	});
+}
+function d51(){
+	layer.alert('链：http://pan.baidu.com/s/1bprPEof   密：dvm5', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'UPUPW Nginx(32位)下载链接',
+	});
+}
+function d52(){
+	layer.alert('链：http://pan.baidu.com/s/1i4DbVMX   密：si5o', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'UPUPW Nginx(64位)下载链接',
+	});
+}
+function d53(){
+	layer.alert('链：http://pan.baidu.com/s/1kU6Snt1   密：c64e', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'WampServer (32bit)2.5下载链接',
+	});
+}
+function d54(){
+	layer.alert('链：http://pan.baidu.com/s/1dFcS7mT   密：by3u', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'WampServer 1.6.1.33下载链接',
+	});
+}
+function d55(){
+	layer.alert('链：http://pan.baidu.com/s/1b42HYI   密：6vrc', {
+	  skin: 'layui-layer-molv' //样式类名
+	  ,closeBtn: 0,
+	  title:'XAMPP 3.2.2.0下载链接',
+	});
+}
